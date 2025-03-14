7b79cb88611959c84234ec6bc8f69af132cd725c
rahmaa4 <rahmagudal@gmail.com>
2025-03-14 14:57:26 +0000

    add nav toggle theme functionality

--- src/App/App.tsx
+++ src/App/App.tsx
@@ -2,11 +2,27 @@ import { useContext, useEffect } from "react";
 import Nav from "../Components/Nav/Nav";
 import { ThemeContext } from "../Contexts/Theme/ThemeContext";
 
+
+const bgImageLight = '/assets/images/bg-noise-light.png';
+const bgImageDark = '/assets/images/bg-noise-dark.png';
+
 export default function App() {
     const { isLight } = useContext(ThemeContext);
 
     useEffect(() => {
-        document.documentElement.style.backgroundColor = isLight ? '#fff' : '#12131A'
+        document.documentElement.style.backgroundImage = `url(${bgImageLight})`;
+        document.documentElement.style.backgroundSize = 'cover';
+    }, []);
+
+    useEffect(() => {
+        if (isLight) {
+        document.documentElement.style.backgroundImage = `url(${bgImageLight})`;
+        document.documentElement.style.backgroundColor = '#fff'
+            
+        } else {
+            document.documentElement.style.backgroundColor = '#12131A'
+            document.documentElement.style.backgroundImage = `url(${bgImageDark})`;
+        }
     }, [isLight]);
 
     return (
